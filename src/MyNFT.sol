d278ed0ef0fa9bfdc95e544d985701898371db6c
gmh5225 <2315157@qq.com>
2024-10-15 17:47:48 +0800

    mynft

---
+++ src/MyNFT.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    uint256 private NextTokenId;
+    uint256 private MAX_SUPPLY;
+    uint256 public MintSupplyForUsers;
+
+    error maxSupplyReached();
+
+    constructor(address initialOwner) ERC721("MyNFT", "MFT") Ownable(initialOwner) {
+        MAX_SUPPLY = 1000;
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        if (NextTokenId >= MAX_SUPPLY) {
+            revert maxSupplyReached();
+        }
+
+        uint256 tokenId = NextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following function for self mint without owner legibility
+    function safeMintForUsers(address to, string memory uri) public {
+        if (NextTokenId >= MAX_SUPPLY) {
+            revert maxSupplyReached();
+        }
+
+        uint256 tokenId = NextTokenId++;
+        MintSupplyForUsers++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    )
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ test/MyNFT.t.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "forge-std/Test.sol";
+import "../src/MyNFT.sol";
+
+contract MyNFTTest is Test {
+    MyNFT public nft;
+    address public owner;
+    address public user1;
+    address public user2;
+
+    function setUp() public {
+        owner = address(this);
+        user1 = address(0x1);
+        user2 = address(0x2);
+        nft = new MyNFT(owner);
+    }
+
+    function testInitialState() public {
+        assertEq(nft.name(), "MyNFT");
+        assertEq(nft.symbol(), "MFT");
+        assertEq(nft.MintSupplyForUsers(), 0);
+    }
+
+    function testSafeMint() public {
+        vm.prank(owner);
+        nft.safeMint(user1, "ipfs://test-uri-1");
+        
+        assertEq(nft.balanceOf(user1), 1);
+        assertEq(nft.ownerOf(0), user1);
+        assertEq(nft.tokenURI(0), "ipfs://test-uri-1");
+        assertEq(nft.MintSupplyForUsers(), 0);
+    }
+
+    function testSafeMintForUsers() public {
+        vm.prank(user1);
+        nft.safeMintForUsers(user1, "ipfs://test-uri-2");
+        
+        assertEq(nft.balanceOf(user1), 1);
+        assertEq(nft.ownerOf(0), user1);
+        assertEq(nft.tokenURI(0), "ipfs://test-uri-2");
+        assertEq(nft.MintSupplyForUsers(), 1);
+    }
+
+    function testMaxSupply() public {
+        for (uint i = 0; i < 1000; i++) {
+            vm.prank(user1);
+            nft.safeMintForUsers(user1, string(abi.encodePacked("ipfs://test-uri-", vm.toString(i))));
+        }
+        
+        assertEq(nft.MintSupplyForUsers(), 1000);
+        
+        vm.expectRevert(MyNFT.maxSupplyReached.selector);
+        vm.prank(user2);
+        nft.safeMintForUsers(user2, "ipfs://test-uri-1001");
+    }
+
+    function testOnlyOwnerCanSafeMint() public {
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, user1));
+        nft.safeMint(user1, "ipfs://test-uri-3");
+    }
+
+    function testSupportsInterface() public {
+        assertTrue(nft.supportsInterface(type(IERC721).interfaceId));
+        assertTrue(nft.supportsInterface(type(IERC721Metadata).interfaceId));
+        assertTrue(nft.supportsInterface(type(IERC721Enumerable).interfaceId));
+    }
+}
